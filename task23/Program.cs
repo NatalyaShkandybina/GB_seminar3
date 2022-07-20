f4f694caddf9d7500c70a328006902ae03af3ca0
Natalya Shkandybina <natalya.shkandybina@mail.ru>
2022-07-20 22:10:21 +0300

    Info about task 23

---
+++ task23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Таблица кубов: ");
+for (int i = 1; i <= number; i++){
+    Console.Write($"{i*i*i} ");
+}
